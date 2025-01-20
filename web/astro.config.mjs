b96865bf9295f395e117115f24b3986a1a295a74
Tyler Evans <snaveevans@users.noreply.github.com>
2025-01-20 08:30:29 -0700

    add workaround for astro bug

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -14,5 +14,15 @@ export default defineConfig({
   }),
 
   integrations: [react()],
-});
 
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      // @ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
+});
